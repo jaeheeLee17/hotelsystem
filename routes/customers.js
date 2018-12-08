bd9a1a6be7070bd5e3f78b09450264f30933b861
leejaehee <ipod031@naver.com>
2018-12-08 13:07:13 +0900

    synchronize environment

--- routes/customers.js
+++ routes/customers.js
@@ -1,21 +1,27 @@
 var express = require('express')
 var app = express()
+var isAuthenticated = function (req, res, next) {
+	if (req.isAuthenticated())
+	  return next();
+	res.redirect('/login');
+  };
+
 
 // SHOW LIST OF USERS
-app.get('/', function(req, res, next) {
+app.get('/',isAuthenticated, function(req, res, next) {
 	req.getConnection(function(error, conn) {
 		conn.query('SELECT * FROM customer ORDER BY id DESC',function(err, rows, fields) {
 			//if(err) throw err
 			if (err) {
 				req.flash('error', err)
 				res.render('customers/list', {
-					title: 'Customer List',
+					title: 'Customer List', 
 					data: ''
 				})
 			} else {
 				// render to views/user/list.ejs template file
 				res.render('customers/list', {
-					title: 'Customer List',
+					title: 'Customer List', 
 					data: rows
 				})
 			}
@@ -24,7 +30,7 @@ app.get('/', function(req, res, next) {
 })
 
 // SHOW ADD USER FORM
-app.get('/add', function(req, res, next){
+app.get('/add',isAuthenticated, function(req, res, next){	
 	// render to views/user/add.ejs
 	res.render('customers/add', {
 		title: 'Add New Customer',
@@ -34,22 +40,22 @@ app.get('/add', function(req, res, next){
 		car: '',
 		nation: '',
 		phone: '',
-		email: ''
+		email: ''		
 	})
 })
 
 // ADD NEW USER POST ACTION
-app.post('/add', function(req, res, next){
+app.post('/add',isAuthenticated, function(req, res, next){	
 	req.assert('name', 'Name is required').notEmpty()           //Validate name
     // req.assert('email', 'A valid email is required').isEmail()  //Validate email
 
     var errors = req.validationErrors()
-
+    
     if( !errors ) {   //No errors were found.  Passed Validation!
-
+		
 		/********************************************
 		 * Express-validator module
-
+		 
 		req.body.comment = 'a <span>comment</span>';
 		req.body.username = '   a user    ';
 
@@ -60,18 +66,18 @@ app.post('/add', function(req, res, next){
 			id: req.sanitize('id').escape().trim(),
 			password: req.sanitize('password').escape().trim(),
 			name: req.sanitize('name').escape().trim(),
-			car: req.body.car ? true: false,
+			car: req.sanitize('car').escape().trim(),
 			nation: req.sanitize('nation').escape().trim(),
 			phone: req.sanitize('phone').escape().trim(),
 			email: req.sanitize('email').escape().trim()
 		}
-
+		
 		req.getConnection(function(error, conn) {
 			conn.query('INSERT INTO customer SET ?', user, function(err, result) {
 				//if(err) throw err
 				if (err) {
 					req.flash('error', err)
-
+					
 					// render to views/user/add.ejs
 					res.render('customers/add', {
 						title: 'Add New Customer',
@@ -81,11 +87,11 @@ app.post('/add', function(req, res, next){
 						car: user.car,
 						nation: user.nation,
 						phone: user.phone,
-						email: user.email
+						email: user.email					
 					})
-				} else {
+				} else {				
 					req.flash('success', 'Data added successfully!')
-
+					
 					// render to views/user/add.ejs
 					res.render('customers/add', {
 						title: 'Add New Customer',
@@ -95,7 +101,7 @@ app.post('/add', function(req, res, next){
 						car: '',
 						nation: '',
 						phone: '',
-						email: ''
+						email: ''					
 					})
 				}
 			})
@@ -105,13 +111,13 @@ app.post('/add', function(req, res, next){
 		var error_msg = ''
 		errors.forEach(function(error) {
 			error_msg += error.msg + '<br>'
-		})
-		req.flash('error', error_msg)
-
+		})				
+		req.flash('error', error_msg)		
+		
 		/**
-		 * Using req.body.name
+		 * Using req.body.name 
 		 * because req.param('name') is deprecated
-		 */
+		 */ 
         res.render('customers/add', {
 			title: 'Add New Customer',
 			id: user.id,
@@ -120,17 +126,17 @@ app.post('/add', function(req, res, next){
 			car: user.car,
 			nation: user.nation,
 			phone: user.phone,
-			email: user.email
+			email: user.email					
 		})
     }
 })
 
 // SHOW EDIT USER FORM
-app.get('/edit/(:id)', function(req, res, next){
+app.get('/edit/(:id)',isAuthenticated, function(req, res, next){
 	req.getConnection(function(error, conn) {
 		conn.query("SELECT * FROM customer WHERE id = ?", req.params.id, function(err, rows, fields) {
 			if(err) throw err
-
+			
 			// if user not found
 			if (rows.length <= 0) {
 				req.flash('error', 'User not found with id = ' + req.params.id)
@@ -139,7 +145,7 @@ app.get('/edit/(:id)', function(req, res, next){
 			else { // if user found
 				// render to views/user/edit.ejs template file
 				res.render('customers/edit', {
-					title: 'Edit customer',
+					title: 'Edit customer', 
 					//data: rows[0],
 					id: rows[0].id,
 					password: rows[0].password,
@@ -147,26 +153,26 @@ app.get('/edit/(:id)', function(req, res, next){
 					car: rows[0].car,
 					nation: rows[0].nation,
 					phone: rows[0].phone,
-					email: rows[0].email
+					email: rows[0].email					
 				})
-			}
+			}			
 		})
 	})
 })
 
 // EDIT USER POST ACTION
-app.put('/edit/(:id)', function(req, res, next) {
+app.put('/edit/(:id)',isAuthenticated, function(req, res, next) {
 	req.assert('name', 'Name is required').notEmpty()           //Validate name
-
+	
     // req.assert('email', 'A valid email is required').isEmail()  //Validate email
 
     var errors = req.validationErrors()
-
+    
     if( !errors ) {   //No errors were found.  Passed Validation!
-
+		
 		/********************************************
 		 * Express-validator module
-
+		 
 		req.body.comment = 'a <span>comment</span>';
 		req.body.username = '   a user    ';
 
@@ -177,18 +183,18 @@ app.put('/edit/(:id)', function(req, res, next) {
 			id: req.sanitize('id').escape().trim(),
 			password: req.sanitize('password').escape().trim(),
 			name: req.sanitize('name').escape().trim(),
-			car: req.body.car ? true: false,
+			car: req.sanitize('car').escape().trim(),
 			nation: req.sanitize('nation').escape().trim(),
 			phone: req.sanitize('phone').escape().trim(),
 			email: req.sanitize('email').escape().trim()
 		}
-
+		
 		req.getConnection(function(error, conn) {
 			conn.query("UPDATE CUSTOMER SET ? WHERE id = '" + req.params.id +"'", user, function(err, result) {
 				//if(err) throw err
 				if (err) {
 					req.flash('error', err)
-
+					
 					// render to views/user/add.ejs
 					res.render('customers/edit', {
 						title: 'EDIT Customer',
@@ -202,7 +208,7 @@ app.put('/edit/(:id)', function(req, res, next) {
 					})
 				} else {
 					req.flash('success', 'Data updated successfully!')
-
+					
 					// render to views/user/add.ejs
 					res.render('customers/edit', {
 						title: 'Edit Customer',
@@ -224,12 +230,12 @@ app.put('/edit/(:id)', function(req, res, next) {
 			error_msg += error.msg + '<br>'
 		})
 		req.flash('error', error_msg)
-
+		
 		/**
-		 * Using req.body.name
+		 * Using req.body.name 
 		 * because req.param('name') is deprecated
-		 */
-        res.render('customers/edit', {
+		 */ 
+        res.render('customers/edit', { 
             title: 'Edit Customer',
 			id: req.params.id,
 			password: req.body.password,
@@ -243,9 +249,9 @@ app.put('/edit/(:id)', function(req, res, next) {
 })
 
 // DELETE USER
-app.delete('/delete/(:id)', function(req, res, next) {
+app.delete('/delete/(:id)',isAuthenticated, function(req, res, next) {
 	var user = { id: req.params.id }
-
+	
 	req.getConnection(function(error, conn) {
 		conn.query('DELETE FROM customer WHERE id = ' + req.params.id, user, function(err, result) {
 			//if(err) throw err
